bf730476169ead34512c43371b5ddef203e0bd3b
Dekel Avrahami <dekel@lightricks.com>
2020-08-27 15:32:51 +0300

    Mantle: delete MTLManagedObjectAdapter.
    It is not in use and it uses deprecated iOS API.

--- Mantle/Mantle.h
+++ Mantle/Mantle.h
@@ -15,7 +15,6 @@ FOUNDATION_EXPORT double MantleVersionNumber;
 FOUNDATION_EXPORT const unsigned char MantleVersionString[];
 
 #import <Mantle/MTLJSONAdapter.h>
-#import <Mantle/MTLManagedObjectAdapter.h>
 #import <Mantle/MTLModel.h>
 #import <Mantle/MTLModel+NSCoding.h>
 #import <Mantle/MTLValueTransformer.h>
